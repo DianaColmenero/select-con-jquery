0254595e859ba10b2c65ebede4ee6fbeeb170d7e
Developer <developer@IM-M11-877.local>
2018-06-18 16:58:50 -0500

    update duplicate elements

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ var paintSelect = function() {
 
 var loadStates = function(selectedState) {
 	cleanSelect();
-	  data[selectedState].forEach((state, i) => {
+	  data[selectedState].forEach((element, i) => {
 		$('#city').append('<option value="">' + data[selectedState][i] + '</option>');//Creo elementos HTML desde el DOM
 	});
 }
@@ -31,30 +31,83 @@ var addCity = function(e) {
   var mensaje = $('#city')
 		.find(':selected')
 		.text();
-	if (newArray.length > 4) {
+	if (newArray.length > 9) {
 		newArray.shift();
 		// console.log('TEST');
 	}
 
 	cityContainer(mensaje);
 	// getDuplicatedValues(newArray);
+	paintList();
 }
   var newArray = [];
-
-  var cityContainer = function(mensaje){
-		//inserto elementos seleccionados en el array vacio
-		newArray.push(mensaje);
-
-		myFunction(newArray);
   
-  }
-
-  var myFunction = function(newArray){
+  var cityContainer = function(mensaje){
+	  //inserto elementos seleccionados en el array vacio
+	  newArray.push(mensaje);
+	  
+	  myFunction(newArray);
+	  
+	}
+	
+// 	getDuplicatedValues = function(newArray) {
+// 		// console.log('prub');
 		
-    var $contenedorLista = $('#lista');
-    $contenedorLista.empty();
-    for (var i = 0; i < newArray.length; i++){
-      var $crearLi = $('<p class="lista" />');//Creo elementos HTML desde el DOM
+// 		var current = null;
+// 		var count = 0;
+
+// 		for(var i = 0; i < newArray.length; i++){
+//     		if(newArray[i] !== current){
+//     		if(count > 0){
+				
+// 				$('#elemDup').html(current + " " + count);
+//     }
+//     	current = newArray[i];
+// 		count = 1;
+	
+//   	}else{
+// 		count++;
+		
+//   	}
+			
+// 	}
+
+// }
+
+Array.prototype.getDuplicatedValues = function(newArray) {
+
+	var counter = 0;
+	for (var i = 0; i < this.length; i++) {
+		// console.log(this);
+		
+		for (var j = 0; j < i; j++) {
+			// console.log(this);
+			
+			if (i != j && this[i] === this[j]) {
+				counter++;
+				break;
+			}
+		}
+	}
+	return counter;
+}
+
+
+function paintList() {
+	// console.log('prub');
+	
+	var lista = newArray;
+	var total = lista.getDuplicatedValues();
+	$('#elemDup').text(total);
+}
+
+
+	var myFunction = function(newArray){
+		
+		var $contenedorLista = $('#lista');
+		$contenedorLista.empty();
+		for (var i = 0; i < newArray.length; i++){
+			var $crearLi = $('<p class="lista" />');//Creo elementos HTML desde el DOM
       $contenedorLista.append($crearLi);
       $crearLi.text(newArray[i]);
   
@@ -62,17 +115,6 @@ var addCity = function(e) {
   
   }
 
-	// Array.prototype.getDuplicatedValues = function(newArray) {
-	// 	console.log('prub');
-		
-	// 	var counter = 0;
-	// 	for (var i = 0; i < newArray.length; i++) {
-			
-	// 	}
-	// 	return counter;
-	// 	console.log(counter);
-		
-	// }
 
 var orderByName = function() {
 newArray = newArray.sort();
@@ -91,62 +133,5 @@ var orderByLength = function(arr, asc) {
 }
 
 
-
-
-//$('#lista li').each(function(i, v){
- // console.log($(v).text());
-  
-//});
-
-//console.log(data.length);
-
-// var items = $('#lista li');
-
-//   for(var i = 0; i < items.length; i++){
-//     newArray.push(items[i].html);
-//   }
-
-//console.log(newArray.length);
-
-
-//console.log(newArray);
-//or (mismo resultado)
-//console.log(data['Jalisco']);
-
-//log de object con map
-// var otherData = (data['Aguascalientes']).map(function(city) {
-//   return [city]; //devuelve todos los municipios
-// });
-//console.log(otherData);
-
-
-//esto no se puede ya que .map() esta esperando una funcion:
-//var otherData = (data['Aguascalientes']).map(newArray);
-
-
-//log de object con push
-// var otherData = (data['Jalisco']).push(function(city) {
-//   return [(city)];
-// });
-//console.log(otherData);  me devve el numero 9
-//or
-// var otherData = (data['Jalisco']).push(newArray);
-//console.log(otherData); me devve el numero 10
-
-// var ejemplo = 'este es un ejemplo de string';
-//console.log(ejemplo.length);
-//console.log(ejemplo.toUpperCase());
-
-// var resultArr = Object.keys(data).map(function(key) {
-//   return [data[key]];
-// });
-
-//console.log(resultArr);
-
-//var prub = data.sort();
-//console.log(prub);
-
-
-// var otroArr = ['Diana', 29, 'Java Script']
 
 
